c316cb22e098184097f436db637fe7cf5c2e6085
Sudharshan <solderneer@gmail.com>
2020-07-31 11:00:08 +0800

    feat: added calendar for schedule view

--- lib/pages/main/schedule_view.dart
+++ lib/pages/main/schedule_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../../components/appbar.dart';
 import '../../components/list_items.dart';
@@ -10,14 +11,233 @@ class ScheduleView extends StatefulWidget {
   _ScheduleViewState createState() => _ScheduleViewState();
 }
 
-class _ScheduleViewState extends State<ScheduleView> {
+class _ScheduleViewState extends State<ScheduleView>
+    with TickerProviderStateMixin {
+  Map<DateTime, Map<String, dynamic>> _events;
+  List _selectedEvents;
+  AnimationController _animationController;
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+    final _selectedDay = DateTime.now();
+
+    _events = {
+      _selectedDay.subtract(Duration(days: 30)): {
+        "image": "Event A0",
+        "duration": "2 - 4pm",
+        "topic": "Physics",
+        "tutorname": "Arvinderjit Singh"
+      },
+      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
+      _selectedDay.subtract(Duration(days: 20)): [
+        'Event A2',
+        'Event B2',
+        'Event C2',
+        'Event D2'
+      ],
+      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
+      _selectedDay.subtract(Duration(days: 10)): [
+        'Event A4',
+        'Event B4',
+        'Event C4'
+      ],
+      _selectedDay.subtract(Duration(days: 4)): [
+        'Event A5',
+        'Event B5',
+        'Event C5'
+      ],
+      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      _selectedDay.add(Duration(days: 1)): [
+        'Event A8',
+        'Event B8',
+        'Event C8',
+        'Event D8'
+      ],
+      _selectedDay.add(Duration(days: 3)):
+          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+      _selectedDay.add(Duration(days: 7)): [
+        'Event A10',
+        'Event B10',
+        'Event C10'
+      ],
+      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
+      _selectedDay.add(Duration(days: 17)): [
+        'Event A12',
+        'Event B12',
+        'Event C12',
+        'Event D12'
+      ],
+      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
+      _selectedDay.add(Duration(days: 26)): [
+        'Event A14',
+        'Event B14',
+        'Event C14'
+      ],
+    };
+
+    _selectedEvents = _events[_selectedDay] ?? [];
+    _calendarController = CalendarController();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _calendarController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Container(
-            padding: EdgeInsets.only(left: 20.0, right: 10.0, top: 8.0),
+            padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 8.0),
             child: ListView(shrinkWrap: true, children: <Widget>[
-              AxiomAppBar(),
+              Padding(
+                  padding: EdgeInsets.only(left: 10.0), child: AxiomAppBar()),
+              _buildTableCalendar(),
             ])));
+  }
+
+  /************************************** CALLBACKS *****************************/
+
+  void _onDaySelected(DateTime day, List events) {
+    print('CALLBACK: _onDaySelected');
+    setState(() {
+      _selectedEvents = events;
+    });
+  }
+
+  void _onVisibleDaysChanged(
+      DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onVisibleDaysChanged');
+  }
+
+  void _onCalendarCreated(
+      DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onCalendarCreated');
+  }
+
+  /***************************** BUILDER UTILITIES *****************************/
+  Widget _buildTableCalendar() {
+    return TableCalendar(
+      calendarController: _calendarController,
+      events: _events,
+      initialCalendarFormat: CalendarFormat.month,
+      formatAnimation: FormatAnimation.slide,
+      startingDayOfWeek: StartingDayOfWeek.sunday,
+      availableGestures: AvailableGestures.all,
+      calendarStyle: CalendarStyle(
+        outsideDaysVisible: false,
+        weekendStyle:
+            TextStyle().copyWith(color: Theme.of(context).primaryColor),
+        holidayStyle:
+            TextStyle().copyWith(color: Theme.of(context).primaryColor),
+      ),
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekendStyle:
+            TextStyle().copyWith(color: Theme.of(context).primaryColor),
+      ),
+      headerStyle: HeaderStyle(
+        centerHeaderTitle: true,
+        formatButtonVisible: false,
+      ),
+      builders: CalendarBuilders(
+        selectedDayBuilder: (context, date, _) {
+          return FadeTransition(
+            opacity: Tween(begin: 0.0, end: 1.0).animate(_animationController),
+            child: Container(
+              margin: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                  color: Color(0xFFF0E8FA),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              width: 100,
+              height: 100,
+              child: Center(
+                  child: Text(
+                '${date.day}',
+                style: TextStyle().copyWith(fontSize: 14.0),
+              )),
+            ),
+          );
+        },
+        todayDayBuilder: (context, date, _) {
+          return Container(
+            margin: const EdgeInsets.all(4.0),
+            decoration: BoxDecoration(
+                color: Color(0xFFF1F1F1),
+                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+            width: 100,
+            height: 100,
+            child: Center(
+                child: Text(
+              '${date.day}',
+              style: TextStyle()
+                  .copyWith(fontSize: 14.0, fontWeight: FontWeight.w500),
+            )),
+          );
+        },
+        markersBuilder: (context, date, events, holidays) {
+          final children = <Widget>[];
+
+          if (events.isNotEmpty) {
+            events.forEach(
+              (event) => children.add(Container(
+                margin: EdgeInsets.all(1.0),
+                width: 5,
+                height: 5,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    borderRadius: BorderRadius.all(Radius.circular(2.5))),
+              )),
+            );
+          }
+
+          return <Widget>[
+            Positioned(bottom: 6.0, child: Row(children: children))
+          ];
+        },
+      ),
+      onDaySelected: (date, events) {
+        _onDaySelected(date, events);
+        _animationController.forward(from: 0.0);
+      },
+      onVisibleDaysChanged: _onVisibleDaysChanged,
+      onCalendarCreated: _onCalendarCreated,
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: _calendarController.isSelected(date)
+            ? Colors.brown[500]
+            : _calendarController.isToday(date)
+                ? Colors.brown[300]
+                : Colors.blue[400],
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
   }
 }
